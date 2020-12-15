4479135974ca406a8ec7792f2c6cca62dce63f6c
Yufan11 <49989012+Yufan11@users.noreply.github.com>
2020-12-15 04:34:27 +0100

    Update pl_module.py

--- src/mot_neural_solver/pl_module/pl_module.py
+++ src/mot_neural_solver/pl_module/pl_module.py
@@ -142,7 +142,7 @@ class MOTNeuralSolver(pl.LightningModule):
         head_factor = self.hparams['graph_model_params']['attention']['attention_head_num']
         num_steps_attention = len(outputs['att_coefficients'])
         
-        att_statistics = torch.empty(size=(5,head_factor,num_steps_attention)).cuda()
+        att_statistics = torch.empty(size=(7,head_factor,num_steps_attention)).cuda()
         ### attention loss matrix ### 
         positive_vals = batch.edge_labels.sum()
         if positive_vals:
@@ -152,8 +152,8 @@ class MOTNeuralSolver(pl.LightningModule):
         for step in range(num_steps_attention):
             for head in range(head_factor):
                 att_statistics[0,head,step] = F.binary_cross_entropy_with_logits(outputs['att_coefficients'][step][head].view(-1),
-                                                                              batch.edge_labels.view(-1),
-                                                                              pos_weight= pos_weight)  
+                                                                              batch.edge_labels,
+                                                                              pos_weight= pos_weight) 
         ### attention loss matrix ###
         
         ### attention mean matrix ###
@@ -179,21 +179,51 @@ class MOTNeuralSolver(pl.LightningModule):
             for head in range(head_factor):
                 att_statistics[4,head,step] = torch.max(outputs['att_coefficients'][step][head].view(-1))        
         ### attention maximum matrix ###
-
+        
+        
+        x, idx, edge_attr,label = batch.x, batch.edge_index, batch.edge_attr, batch.edge_labels
+        ### topk accuracy matrix ###
+        for step in range(num_steps_attention):
+            for head in range(head_factor):
+                a = outputs['att_coefficients'][step][head].view(-1)
+                accuracy = 0
+                for center in idx[0][0:10]:
+                    mask = (center == idx[0])
+                    a[~mask] = -1 
+                    _, topk_mask = torch.topk(a,5)
+                    accuracy += torch.sum(label[topk_mask])/2 
+                accuracy /= 10
+                att_statistics[5,head,step] = accuracy
         val_outputs["att_statistics"] = att_statistics
+        ### topk accuracy matrix ###
+        
+        ### node embedding difference matrix ###
+        for step in range(num_steps_attention):
+            for head in range(head_factor):
+                a = outputs['att_coefficients'][step][head].view(-1)
+                rate = 0
+                for center in idx[0][0:10]:
+                    mask = (center == idx[0])
+                    a[~mask] = -1 
+                    _, topk_mask = torch.topk(a,5)
+                    att_neighbours = idx[1][topk_mask]
+                    dis1 = torch.sum(torch.square(center-x[att_neighbours]))/5
+                    dis2 = torch.norm(center-x[att_neighbours],dim=1,p=None)
+                    value,_ = torch.topk(dis2,5)
+                    dis2 = torch.sum(value)
+                    rate += dis1/dis2
+                rate /= 10
+        val_outputs["att_statistics"] = att_statistics
+        ### node embedding difference matrix ###
         return val_outputs
 
     def validation_epoch_end(self, val_outputs):
-        att_statistics = 0
-        for out in val_outputs:
-            att_statistics += out["att_statistics"]
-        att_statistics /= len(val_outputs)
-        quantity = ['loss','mean','variance','min','max']
+        att_statistics = val_outputs[-1]["att_statistics"]
+        quantity = ['loss','mean','variance','min','max','topk accuracy','emb_diff']
         t = 0
         for i in quantity:
             print(i,':',att_statistics[t])
             t += 1
-
         metrics = pd.DataFrame(val_outputs).mean(axis=0).to_dict()
         metrics = {metric_name: torch.as_tensor(metric) for metric_name, metric in metrics.items()}
         return {'val_loss': metrics['loss/val'], 'log': metrics}
