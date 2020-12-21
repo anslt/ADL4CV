fa248f6fa9c088e72b861b5d684326c94c512a25
anslt <ge75wed@mytum.de>
2020-12-21 15:40:29 +0100

    fix bug of visual

--- src/mot_neural_solver/pl_module/pl_module.py
+++ src/mot_neural_solver/pl_module/pl_module.py
@@ -1,4 +1,4 @@
-import os
+import osimport os
 import os.path as osp
 
 import pandas as pd
@@ -6,6 +6,7 @@ import pandas as pd
 from torch_geometric.data import DataLoader
 
 import torch
+import numpy as np
 
 from torch import optim as optim_module
 from torch.optim import lr_scheduler as lr_sched_module
@@ -20,6 +21,9 @@ from mot_neural_solver.path_cfg import OUTPUT_PATH
 from mot_neural_solver.utils.evaluation import compute_perform_metrics
 from mot_neural_solver.tracker.mpn_tracker import MPNTracker
 
+import matplotlib
+import matplotlib.pyplot as plt
+
 class MOTNeuralSolver(pl.LightningModule):
     """
     Pytorch Lightning wrapper around the MPN defined in model/mpn.py.
@@ -32,7 +36,7 @@ class MOTNeuralSolver(pl.LightningModule):
 
         self.hparams = hparams
         self.validation_epoch = 0
-        self.has_trained = False
+        self.has_trained = True
         self.model, self.cnn_model = self.load_model()
     
     def forward(self, x):
@@ -110,17 +114,19 @@ class MOTNeuralSolver(pl.LightningModule):
                                                             pos_weight= pos_weight)
         
         att_loss_matrix = None
+        att_regu_strength = self.hparams['graph_model_params']['attention']['att_regu_strength']
+        att_loss=0
         if att_regu:
             num_steps_attention = len(outputs['att_coefficients'])
             head_factor = self.hparams['graph_model_params']['attention']['attention_head_num']
-            att_regu_strength = self.hparams['graph_model_params']['attention']['att_regu_strength']
             att_loss_matrix = torch.empty(size=(head_factor, num_steps_attention)).cuda()
             for step in range(num_steps_attention):
                 for head in range(head_factor):
+                    weight = batch.edge_labels.view(-1) == 0 + (batch.edge_labels.view(-1) == 1) * pos_weight
                     att_loss_matrix[head, step] = F.binary_cross_entropy(
                         outputs['att_coefficients'][step][head].view(-1),
                         batch.edge_labels.view(-1),
-                        pos_weight=pos_weight)
+                        weight=weight)
             att_loss = torch.sum(att_loss_matrix) / head_factor
         return {"loss": loss_class + att_regu_strength * att_loss , "loss_class": loss_class, "loss_matrix" : att_loss_matrix}
 
@@ -136,7 +142,9 @@ class MOTNeuralSolver(pl.LightningModule):
         return {'loss': loss, 'log': log}
 
     def training_epoch_end(outputs):
-        if self.has_trained is False:
+        if self.has_trained == False:
+            print("++++++++++++++")
+            print("has trained is true.")
             self.has_trained = True
 
     def validation_step(self, batch, batch_idx):
@@ -148,10 +156,10 @@ class MOTNeuralSolver(pl.LightningModule):
         logs = {**compute_perform_metrics(outputs, batch), **{'loss': loss}}
         log = {key + '/val': val for key, val in logs.items()} 
 
-        if self.has_trained = True:
+        if self.has_trained == True:
 
-            val_outputs["log"] = log
-            val_outputs["edge_attr"] = batch.edge_attr.detach().transpose(0,1).contingous()
+            val_outputs = {"log": log}
+            val_outputs["edge_attr"] = batch.edge_attr.detach()
             val_outputs["loss_matrix"] = loss_dic["loss_matrix"].detach()
             val_outputs["loss_class"] = loss_dic["loss_class"].detach()
             val_outputs["loss"] = loss_dic["loss"].detach()
@@ -163,11 +171,12 @@ class MOTNeuralSolver(pl.LightningModule):
             head_factor = self.hparams['graph_model_params']['attention']['attention_head_num']
             num_steps_attention = len(outputs['att_coefficients'])
             
-
-            edge_id = batch.edge_index.detach().transpose(0,1).contingous()
-            attr = val_outputs["edge_attr"]
-            labels = betch.labels.detach()
-
+            idx = batch.tracking_id.cpu()
+            edge_id = batch.edge_index.detach().transpose(0,1).contiguous().cpu()
+            attr = val_outputs["edge_attr"].cpu()
+            labels = batch.edge_labels.detach().cpu()
+            attention = attention.cpu()
+            
             topk = self.hparams['visual']['topk']
             path = self.hparams['visual']['path']
             row = edge_id[:,0]
@@ -179,7 +188,8 @@ class MOTNeuralSolver(pl.LightningModule):
             attention = attention[ind]
             attr = attr[ind]
             labels = labels[ind]
-
+            
+            #edge_id_cpu = edge_id.cpu()
             row = edge_id[:,0]
             col = edge_id[:,1]
 
@@ -226,6 +236,7 @@ class MOTNeuralSolver(pl.LightningModule):
                 exists_topk = torch.zeros(topk)
                 percentage_topk = torch.zeros(topk)
                 tracking_topk = torch.zeros(topk)
+                cal_num = 0 
 
                 for i in range(output.size(0)):
 
@@ -257,100 +268,11 @@ class MOTNeuralSolver(pl.LightningModule):
             
             
             val_outputs["edge_num"] = cal_num
-            val_outputs["val_res_step"] = val_res_step
-            val_outputs["val_res"] = val_res
-            val_outputs["exists_topk"] = exists_topk
-            val_outputs["tracking_topk"] = tracking_topk
-            """
-            att_statistics = torch.empty(size=(13,head_factor,num_steps_attention)).cuda()
-            positive_vals = batch.edge_labels.sum()
-            if positive_vals:
-                pos_weight = (batch.edge_labels.shape[0] - positive_vals) / positive_vals
-            else: # If there are no positives labels, avoid dividing by zero
-                pos_weight = 0
-            k = 5
-            x, idx, edge_attr,label,time,identity = batch.x, batch.edge_index, batch.edge_attr, batch.edge_labels, batch.frame, batch.tracking_id
-            time = time.float()
-            centers = torch.unique(idx[0])
-            centers = centers[0:100]
-            center_num = len(centers)
-            for step in range(num_steps_attention):
-                for head in range(head_factor):
-                    a = outputs['att_coefficients'][step][head].view(-1)
-                    ### attention loss matrix ### 
-                    att_statistics[0,head,step] = F.binary_cross_entropy_with_logits(outputs['att_coefficients'][step][head].view(-1),
-                                                                                  batch.edge_labels,
-                                                                                  pos_weight= pos_weight) 
-                    ### attention loss matrix ### 
-                    ### attention mean matrix ###
-                    att_statistics[1,head,step] = torch.mean(outputs['att_coefficients'][step][head].view(-1))
-                    ### attention mean matrix ###
-                    ### attention variance matrix ###
-                    att_statistics[2,head,step] = torch.var(outputs['att_coefficients'][step][head].view(-1))  
-                    ### attention variance matrix ###
-                    ### attention minimum matrix ###
-                    att_statistics[3,head,step] = torch.min(outputs['att_coefficients'][step][head].view(-1))        
-                    ### attention minimum matrix ###
-                    ### attention maximum matrix ###
-                    att_statistics[4,head,step] = torch.max(outputs['att_coefficients'][step][head].view(-1))        
-                    ### attention maximum matrix ###
-                    accuracy = 0
-                    dis_rate = 0
-                    time_rate = 0
-                    same = 0
-                    mins = torch.empty(size=(center_num,))
-                    maxs = torch.empty(size=(center_num,))
-                    count = 0
-                    for center in centers:
-                        mask = (center == idx[0])
-                        b = a.clone()
-                        b[~mask] = -1 
-                        _, high_topk_mask = torch.topk(b,k)                         # boolean tensor [M,] M: number of edges
-                        high_topk_neighbors = idx[1][high_topk_mask]                # Long tensor [k,] 
-                        
-                        c = a.clone()
-                        c[~mask] = 2
-                        _, low_topk_mask = torch.topk(c,k,largest= False)
-                        low_topk_neighbors = idx[1][low_topk_mask]
-                        ### min mean, min var, max mean, max var###
-                        mins[count] = torch.min(a[mask])
-                        maxs[count] = torch.max(a[mask])
-                        count += 1
-                        ### min mean, min var, max mean, max var###
-                        ### topk accuracy ###
-                        accuracy += torch.sum(label[high_topk_mask])/2
-                        ### topk accuarcy ###                
-                        ### trajectory ###
-                        same += torch.sum(identity[center] == identity[high_topk_neighbors]) > 0
-                        ### trajectory ###
-                        ### node embedding ###                    
-                        dis1 = torch.sum(torch.norm(x[center]-x[high_topk_neighbors],dim=1))
-                        dis2 = torch.norm(x[center]-x[torch.arange(x.size(0))!=center],dim=1,p=None) 
-                        value,_ = torch.topk(dis2,k,largest= False)
-                        dis2 = torch.sum(value)
-                        dis_rate += dis1/dis2
-                        ### node embedding ###
-                        ### time ###
-                        time_diff_high = torch.mean(torch.abs(time[center]-time[high_topk_neighbors]))
-                        time_diff_low = torch.mean(torch.abs(time[center]-time[low_topk_neighbors]))
-                        time_rate += time_diff_high/time_diff_low
-                        ### time ###
-                        
-                    att_statistics[5,head,step] = torch.mean(mins)
-                    att_statistics[6,head,step] = torch.var(mins)
-                    att_statistics[7,head,step] = torch.mean(maxs)
-                    att_statistics[8,head,step] = torch.var(maxs)             
-                    accuracy /= center_num
-                    att_statistics[9,head,step] = accuracy
-                    same /= center_num
-                    att_statistics[10,head,step] = same
-                    dis_rate /= center_num
-                    att_statistics[11,head,step] = dis_rate
-                    time_rate /= center_num
-                    att_statistics[12,head,step] = time_rate
-                    
-            val_outputs["att_statistics"] = att_statistics
-            """
+            val_outputs["val_res_step"] = val_res_step.to(device)
+            val_outputs["val_res"] = val_res.to(device)
+            val_outputs["exists_topk"] = exists_topk.to(device)
+            val_outputs["tracking_topk"] = tracking_topk.to(device)
+            
         return val_outputs
 
     def validation_epoch_end(self, val_outputs):
@@ -362,7 +284,7 @@ class MOTNeuralSolver(pl.LightningModule):
             print(i,':',att_statistics[t])
             t += 1
         """
-        if self.has_trained = True:
+        if self.has_trained == True:
 
             self.validation_epoch += 1
             edge_attr = []
@@ -378,13 +300,13 @@ class MOTNeuralSolver(pl.LightningModule):
             for val_output in val_outputs:
                 edge_attr += [val_output["edge_attr"]]
                 attention += [val_output["attention"]]
-                edge_num += val_outputs["edge_num"]
-                node_num += val_outputs["node_num"]
+                edge_num += val_output["edge_num"]
+                node_num += val_output["node_num"]
                     
-                val_res_step += val_outputs["val_res_step"]
-                val_res += val_outputs["val_res"]
-                exists_topk += val_outputs["exists_topk"]
-                tracking_topk += val_outputs["tracking_topk"]
+                val_res_step += val_output["val_res_step"]
+                val_res += val_output["val_res"]
+                exists_topk += val_output["exists_topk"]
+                tracking_topk += val_output["tracking_topk"]
 
             val_res_step /= edge_num
             val_res /= edge_num
@@ -396,7 +318,8 @@ class MOTNeuralSolver(pl.LightningModule):
             print(val_res)
             print(exists_topk)
             print(tracking_topk)
-
+            
+            path = self.hparams['visual']['path']
             np.save(path+"val_res_step_"+str(self.validation_epoch)+".npy",val_res_step.detach().cpu().numpy())
             np.save(path+"val_res_"+str(self.validation_epoch)+".npy",val_res.detach().cpu().numpy())
             np.save(path+"exists_topk_"+str(self.validation_epoch)+".npy",exists_topk.detach().cpu().numpy())
@@ -404,60 +327,60 @@ class MOTNeuralSolver(pl.LightningModule):
 
             edge_attr = torch.cat(edge_attr, dim = 0)
             attention = torch.cat(attention, dim = 0)
-            self._plot(edge_attr, attention, self.validation_epoch, self.hparams['visual']['path'], 'last')
-            self._plot(edge_attr, attention, self.validation_epoch, self.hparams['visual']['path'], 'mean')
+            self._plot(edge_attr.cpu(), attention.cpu(), self.validation_epoch, path, 'last')
+            self._plot(edge_attr.cpu(), attention.cpu(), self.validation_epoch, path, 'mean')
 
-        metrics = pd.DataFrame(val_outputs["log"]).mean(axis=0).to_dict()
+        metrics = pd.DataFrame([val["log"] for val in val_outputs]).mean(axis=0).to_dict()
         metrics = {metric_name: torch.as_tensor(metric) for metric_name, metric in metrics.items()}
         return {'val_loss': metrics['loss/val'], 'log': metrics}
 
-    def _plot(attr,attention,epoch,path=None,mode="last"):
-    assert attr.size(1)==6
-    assert mode in ["mean","last"]
-    
-    if mode == "mean":
-        attention = attention.mean(dim=-1)
-    else:
-        attention = attention[:,:,-1]
-    index = [1,2,3,4,0,5]
-    attr=attr[:,index]
-    x_labels=["distance","x_distance","y_distance","log_height_ratio","log_width_ratio","time_difference","embedding_distance"]
-    y_label="attention"
-    k = attention.size(1) ### k need to be 2
-    
-    for i in range(7):
-        if i == 0:
-            x = torch.sqrt(attr[:,0] * attr[:,0] + attr[:,1] * attr[:,1])
+    def _plot(self,attr,attention,epoch,path=None,mode="last"):
+        assert attr.size(1)==6
+        assert mode in ["mean","last"]
+        
+        if mode == "mean":
+            attention = attention.mean(dim=-1)
         else:
-            x = attr[:,i-1]
+            attention = attention[:,:,-1]
+        index = [1,2,3,4,0,5]
+        attr=attr[:,index]
+        x_labels=["distance","x_distance","y_distance","log_height_ratio","log_width_ratio","time_difference","embedding_distance"]
+        y_label="attention"
+        k = attention.size(1) ### k need to be 2
+        
+        for i in range(7):
+            if i == 0:
+                x = torch.sqrt(attr[:,0] * attr[:,0] + attr[:,1] * attr[:,1])
+            else:
+                x = attr[:,i-1]
+                
+            ymax = torch.max(attention)
+            ylim_max = ymax + 0.05 if ymax < 0.95 else 1
             
-        ymax = torch.max(attention)
-        ylim_max = ymax + 0.05 if ymax < 0.95 else 1
-        
-        fig = plt.figure(figsize=(40,25))
-        ax = fig.add_subplot(121)
+            fig = plt.figure(figsize=(40,25))
+            ax = fig.add_subplot(121)
 
-        plt.plot(x, attention[:,0], 'ro', markersize=2)
+            plt.plot(x, attention[:,0], 'ro', markersize=2)
 
-        ax.set_xlabel(x_labels[i], fontsize=30)
-        ax.set_ylabel(y_label, fontsize=30)
-        ax.set_title("mode:" + mode + " ,epoch:" + str(epoch) + " ,node 1. " + x_labels[i] + " vs " + y_label, fontsize=30)
-        ax.set_ylim(0,ylim_max)
-        
-        ax = fig.add_subplot(122)
+            ax.set_xlabel(x_labels[i], fontsize=30)
+            ax.set_ylabel(y_label, fontsize=30)
+            ax.set_title("mode:" + mode + " ,epoch:" + str(epoch) + " ,node 1. " + x_labels[i] + " vs " + y_label, fontsize=30)
+            ax.set_ylim(0,ylim_max)
+            
+            ax = fig.add_subplot(122)
 
-        plt.plot(x, attention[:,1], 'bo', markersize=2)
+            plt.plot(x, attention[:,1], 'bo', markersize=2)
 
-        ax.set_xlabel(x_labels[i], fontsize=30)
-        ax.set_ylabel(y_label, fontsize=30)
-        ax.set_title("mode:" + mode + " ,epoch:" + str(epoch) + " ,node 1. " + x_labels[i] + " vs " + y_label, fontsize=30)
-        ax.set_ylim(0,ylim_max)
-        
-        file = "epoch_" + str(epoch) + "_" + mode + "_" + x_labels[i] + ".png"
-        if path is not None:
-            file = path+file
-        plt.savefig(file)
-        plt.show()
+            ax.set_xlabel(x_labels[i], fontsize=30)
+            ax.set_ylabel(y_label, fontsize=30)
+            ax.set_title("mode:" + mode + " ,epoch:" + str(epoch) + " ,node 1. " + x_labels[i] + " vs " + y_label, fontsize=30)
+            ax.set_ylim(0,ylim_max)
+            
+            file = "epoch_" + str(epoch) + "_" + mode + "_" + x_labels[i] + ".png"
+            if path is not None:
+                file = path+file
+            plt.savefig(file)
+            #plt.show()
 
     def track_all_seqs(self, output_files_dir, dataset, use_gt = False, verbose = False):
         tracker = MPNTracker(dataset=dataset,
